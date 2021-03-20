69fdf34195ac97e2ec595b26386a768e75dbd5d9
JAVIER MARTÍNEZ FERNÁNDEZ <javmarfer3@alum.us.es>
2021-03-20 10:49:24 +0100

    Task 28: Completed
    Modify the controller.

--- src/main/java/acme/features/anonymous/shout/AnonymousShoutController.java
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import acme.entities.shouts.Shout;
 import acme.framework.components.BasicCommand;
 import acme.framework.controllers.AbstractController;
 import acme.framework.entities.Anonymous;
@@ -18,10 +19,14 @@ public class AnonymousShoutController extends AbstractController<Anonymous, Shou
 	@Autowired
 	private AnonymousShoutListService listService;
 	
+	@Autowired
+	private AnonymousShoutCreateService createService;
+	
 	//Constructors
 	@PostConstruct
 	private void initialise() {
 		super.addBasicCommand(BasicCommand.LIST, this.listService);
+		super.addBasicCommand(BasicCommand.CREATE, this.createService);
 	}
 	
 }
